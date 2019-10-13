8f31fab04bb5e7fdc07a760b4d2c37d3b1498fe1
yamaguchi_haruki <harukigit@gmail.com>
2019-10-13 08:51:16 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,10 +9,39 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+
   # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
+  def self.digest(string)
+    #ハッシュ化のためのコストを定義
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
+   #メソッド呼び出し時に引数として渡したstringをハッシュ化
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    #適当な文字列を生成
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    #new_tokenメソッドを呼び出してremember_tokenカラムに代入
+    self.remember_token = User.new_token
+    #remember_digestカラムの値を
+    #上二つのメソッドで生成したランダムなトークンのハッシュで上書き
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+   # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+   # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
