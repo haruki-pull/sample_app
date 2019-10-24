bc09ffe7ceee42c08217ebe4ae187b449ad93147
yamaguchi_haruki <harukigit@gmail.com>
2019-10-24 14:28:50 +0000

    invalidation assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-#config.assets.initialize_on_precompile = false
+
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -14,5 +14,6 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
