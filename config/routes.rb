cc23d333d0de58b8cc354ad97e692b7766091fac
yamaguchi_haruki <harukigit@gmail.com>
2019-10-30 09:45:13 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   
   get 'password_resets/new'
-
   get 'password_resets/edit'
-
   get 'sessions/new'
-
   root 'static_pages#home'
   get  '/help',      to: 'static_pages#help' 
   get  '/about',     to: 'static_pages#about'
@@ -15,8 +12,14 @@ Rails.application.routes.draw do
   get   '/login',    to: 'sessions#new'
   post  '/login',    to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources:users
   resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new,:edit,:create,:update]
+  resources :password_resets,     only: [:new,:edit,:create,:update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,        only: [:create, :destroy]
 end
